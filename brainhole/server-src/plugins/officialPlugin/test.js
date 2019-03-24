ee61a5ba27af793beb76b939d86f91054e05e223
Fmajor <wujinnnnn@gmail.com>
2019-03-25 01:55:16 +0800

    make basic structure of unittest for officialPlugin

--- brainhole/server-src/plugins/officialPlugin/test.js
+++ brainhole/server-src/plugins/officialPlugin/test.js
@@ -1,7 +1,7 @@
 // because the ava test package DO NOT have a global 'before' and 'after' hook
 // i had to concat all test scripts into test-final.js
 // see test-final.js for all the imports
-test.serial.only('Plugin: officialPlugin', async t => {
+test.serial.skip('Plugin: officialPlugin', async t => {
   let tname
   async function testData({r, componentUID, op}) {
     for (let each of r.result) {
@@ -22,7 +22,7 @@ test.serial.only('Plugin: officialPlugin', async t => {
     }
   }
   let uid = 'officialPlugin'
-  let componentUID,r
+  let componentUID, component, r, data, query, origin
   // turn on
   let plugin = globals.plugins.find(_ => _.uid === uid)
   // if you run unittest, the active of all plugins is
@@ -37,17 +37,167 @@ test.serial.only('Plugin: officialPlugin', async t => {
     t.true(plugin.active === true)
   }
   if(tname='test data on and off') {
-    componentUID = `${uid}[data]pluginHookDemo`
-    r = await globals.pluginAPI({operation:'on', uid, component:'data', componentUID})
+    component = 'data'
+    componentUID = `${uid}[${component}]pluginHookDemo`
+    r = await globals.pluginAPI({operation:'on', uid, component, componentUID})
     await testData({r, componentUID, op:'on'})
-    r = await globals.pluginAPI({operation:'off', uid, component:'data', componentUID})
+    r = await globals.pluginAPI({operation:'off', uid, component, componentUID})
     await testData({r, componentUID, op:'off'})
   }
-  if(tname='test hook simularTags') {
+  if(tname='test hook groupRelations') {
+    component = 'hook'
+    componentUID = `${uid}[${component}]groupRelationTag`
+    if(tname='add unitttest data') {
+      data = [
+        {model: "Relation", data: [
+          {name: 'simular'},
+          {name: 'translation'},
+        ]},
+        {model: "Tag", data: [
+          // test simular
+          {name: 'good', relations: [
+            {relation: {name: 'simular'}, from:{name: 'nice'}},
+          ]},
+          {name: 'nice', relations: [
+            {relation: {name: 'simular'}, from:{name: 'great'}},
+          ]},
+          {name: 'great', relations: [
+            {relation: {name: 'simular'}, from:{name: 'fine'}},
+          ]
+          },
+          {name: 'fine'},
+
+          {name: 'bad', relations: [
+            {relation: {name: 'simular'}, from:{name: 'evil'}},
+          ]},
+          {name: 'evil', relations: [
+            {relation: {name: 'simular'}, from:{name: 'awful'}},
+          ]},
+          {name: 'awful'},
+
+          {name: 'hungry'},
+          {name: 'starve'},
+          {name: 'famish'},
+
+          // test translation
+          {name: 'foo(en)', relations:[
+            {relation: {name: 'translation'}, from:{name: 'foo(zh)'}},
+          ]},
+          {name: 'foo(zh)', relations:[
+            {relation: {name: 'translation'}, from:{name: 'foo(jp)'}},
+          ]},
+          {name: 'foo(jp)', relations:[
+            {relation: {name: 'translation'}, from:{name: 'foo(fr)'}},
+          ]},
+          {name: 'foo(fr)'},
+
+          {name: 'bar(en)', relations:[
+            {relation: {name: 'translation'}, from:{name: 'bar(zh)'}},
+          ]},
+          {name: 'bar(zh)', relations:[
+            {relation: {name: 'translation'}, from:{name: 'bar(zh)'}},
+          ]},
+          {name: 'bar(jp)'},
+          {name: 'bar(fr)'},
+
+          {name: 'ha(en)'},
+          {name: 'ha(zh)'},
+          {name: 'ha(jp)'},
+          {name: 'ha(fr)'},
+
+        ]},
+      ]
+      await globals.bulkOP({operation: '+', data})
+    }
+    if(tname='turn on'){
+      await globals.pluginAPI({operation:'on', uid, component, componentUID})
+    }
+
+  }
+  if(0&&(tname='test hook simularTags')) {
+    component = 'hook'
+    componentUID = `${uid}[${component}]simularTags`
+    let hook = plugin.hook.find(_ => _.uid === componentUID)
+    if(tname='add unitttest data') {
+      data = [
+        {model: "Relation", data: [
+          {name: 'simular'},
+        ]},
+        {model: "Tag", data: [
+          // test clarify hook
+          {name: 'good', relations: [
+            {relation: {name: 'simular'}, from:{name: 'nice'}},
+            {relation: {name: 'simular'}, from:{name: 'great'}},
+            {relation: {name: 'simular'}, from:{name: 'fine'}},
+          ]},
+          {name: 'nice'},
+          {name: 'great'},
+          {name: 'fine'},
+
+          {name: 'bad', relations: [
+            {relation: {name: 'simular'}, from:{name: 'evil'}},
+            {relation: {name: 'simular'}, from:{name: 'awful'}},
+          ]},
+          {name: 'evil'},
+          {name: 'awful'},
+
+          {name: 'hungry', relations: [
+            {relation: {name: 'simular'}, from:{name: 'starve'}},
+            {relation: {name: 'simular'}, from:{name: 'famish'}},
+          ]},
+          {name: 'starve'},
+          {name: 'famish'},
+        ]},
+        {model: "Article", data: [
+          // test the next hook
+          {
+            title: `${tname} 1`,
+            tags:[
+              {tag: {name: 'good'}},
+              {tag: {name: 'evil'}},
+              {tag: {name: 'famish'}},
+            ],
+          },
+          {
+            title: `${tname} 2`,
+            tags:[ {tag: {name: 'good'}}]
+          },
+          {
+            title: `${tname} 3`,
+            tags:[ {tag: {name: 'awful'}}]
+          },
+          {
+            title: `${tname} 4`,
+            tags:[ {tag: {name: 'famish'}}]
+          },
+        ]}
+      ]
+      await globals.bulkOP({operation: '+', data})
+    }
+    if(tname='turn on'){
+      await globals.pluginAPI({operation:'on', uid, component, componentUID})
+    }
+    if(0&&(tname='turn off')){
+      await globals.pluginAPI({operation:'off', uid, component, componentUID})
+      await globals.pluginAPI({operation:'on', uid, component, componentUID})
+    }
+    if(tname='add hook'){
+      if(tname='add without field'){
+
+      }
+      if(tname='add with field'){
+
+      }
+      if(tname='modify with field, raise error'){
+
+      }
+      if(tname='delete with field'){
+
+      }
+
+    }
   }
   if(tname='test hook addAncesotrTags') {
-  }
-  if(tname='test hook groupRelations') {
   }
   if(tname='test hook translationTags') {
   }
